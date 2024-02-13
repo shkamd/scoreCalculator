502142772d140ffded91545753c1a9470815a283
shkamd <118361594+shkamd@users.noreply.github.com>
2024-02-13 20:44:59 +0530

    index.html complete

---
+++ script.js
@@ -0,0 +1,63 @@
+var selectedOvers = 4;
+var target;
+
+function onLoadReset(){
+    localStorage.removeItem('selectedOvers')
+    localStorage.removeItem('target');
+}
+
+function changeOvers(condition){
+    const chooseOversInput = document.querySelector('.overs-input');
+    if(condition=='inc'){
+        chooseOversInput.value++;
+    }else
+    if(condition=='dec'){
+        if(chooseOversInput.value>1){
+            chooseOversInput.value--;
+        }
+    }
+    selectedOvers = parseInt(chooseOversInput.value);
+}
+
+const selectInningsDiv = document.querySelector('.select-innings-div');
+const secondInningsDiv = document.querySelector('.second-innings-div');
+
+function showSecondInningsDiv(){
+    selectInningsDiv.style.display = "none";
+    secondInningsDiv.style.display = "block";
+}
+
+function showSelectInningsDiv(){
+    selectInningsDiv.style.display = "block";
+    secondInningsDiv.style.display = "none";
+}
+
+const enterTargetInput = document.querySelector('.enter-target-input');
+
+function startCalculator(condition){
+    if(condition=='first-innings'){
+        localStorage.setItem('selectedOvers',selectedOvers);
+        localStorage.removeItem('target');
+        window.location.href = 'scoreCalculator.html';
+    }else
+    if(condition=='second-innings'){
+        target = enterTargetInput.value;
+        localStorage.setItem('selectedOvers',selectedOvers);
+        localStorage.setItem('target',target);
+        window.location.href = 'scoreCalculator.html';
+    }
+}
+
+const backBtn = document.querySelector('.back-btn');
+const startBtn = document.querySelector('.start-btn');
+
+function enableStartButton(){
+    if(enterTargetInput.value>0){
+        backBtn.style.display = "none";
+        startBtn.style.display = "block";
+    }else
+    if(enterTargetInput.value<1 || enterTargetInput.value==''){
+        backBtn.style.display = "block";
+        startBtn.style.display = "none";
+    }
+}
